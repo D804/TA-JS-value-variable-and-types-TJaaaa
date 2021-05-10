1744b67512c5b3730263fe3105a67283f8db0146
deepak <kushwahadeepakchange121@gmail.com>
2021-05-10 23:38:15 +0530

    assignment1

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -1,27 +1,62 @@
-/* 1. Do the following
+/* 1. Do the following+
 
 1. Declare a variable `user` and take the name using `prompt`. Display the value stored in `user` using `alert`.
 2. Now copy the value of `user` into a new variable named `userName`. Log the value of both `user` and `userName` using `console.log`.
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user =prompt("Enter the username");
+  alert(user);
+let userName = user;
+console.log(user);
+console.log(userName);
+let age =22,isLearning = true;
+ user ="john";
+console.log(user);
+console.log(userName);
+ 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = Number(prompt("Enter a first value"));
+let numB = Number(prompt("Enter a second value"));
+sum =(numA+numB);
+alert(`sum of first value and second value is ${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j=j+1){
+    if(j%2 ==0){
+        console.log(j);
+    }
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for(let oddNumber =9; oddNumber >0; oddNumber=oddNumber-1){
+    console.log(oddNumber);
+}
+
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let i =5;
+while(i>0){
+    console.log(i);
+    i=i-1;
+}
+
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
+let i =10;
+while(i>0){
+    if(i%2 ==0){
+        console.log(i);
+    }
+    
+    i=i-1;
+}
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +71,17 @@ Example:
 "hello", -21; // false
 
 */
+let num1 =Number(prompt("Enter first value"));
+let num2 =Number(prompt("Enter second value"));
+switch(true){
+    case (num1===num2):
+    alert(`true`);
+     break;
+     case (num1!==num2):
+         alert(`false`);
+         break;
+         default:
+             alert(`Enter a valid value`);
+}
+
+
